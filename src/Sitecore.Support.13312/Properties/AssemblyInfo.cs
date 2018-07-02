7bc21d7da1c294b6e9faf3086840a66c4ce9534c
DK\EVS <evs@sitecore.com>
2018-07-02 10:17:06 +0300

    initial commit with fix. routeData.Values["controller"]="Sitecore"

---
+++ src/Sitecore.Support.13312/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13312")]
+[assembly: AssemblyProduct("Sitecore.Support.13312")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.13312/RazorService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sitecore.Support.XA.Foundation.SitecoreExtensions.Services
+{
+  public class RazorService:Sitecore.XA.Foundation.SitecoreExtensions.Services.RazorService
+  {
+    protected class FakeController : Controller
+    {
+    }
+
+    public override string GetRazorViewAsString(object model, string filePath)
+    {
+      StringWriter stringWriter = new StringWriter();
+      HttpContextWrapper httpContext = new HttpContextWrapper(HttpContext.Current);
+      RouteData routeData = new RouteData();
+      routeData.Values["controller"] = "Sitecore";
+      ControllerContext controllerContext = new ControllerContext(new RequestContext(httpContext, routeData), new FakeController());
+      RazorView razorView = new RazorView(controllerContext, filePath, null, false, null);
+      razorView.Render(new ViewContext(controllerContext, razorView, new ViewDataDictionary(model), new TempDataDictionary(), stringWriter), stringWriter);
+      return stringWriter.ToString();
+    }
+  }
+}
