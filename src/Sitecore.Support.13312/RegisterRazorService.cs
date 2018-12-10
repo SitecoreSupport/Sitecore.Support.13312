38c8621a01e2e80922bd9f17e99588d7eea53d0e
iro <iro@sitecore.net>
2018-12-10 18:35:33 +0200

    Make compatible with SXA 1.6

---
+++ src/Sitecore.Support.13312/RegisterRazorService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.XA.Foundation.SitecoreExtensions.Services;
+
+namespace Sitecore.Support
+{
+  public class RegisterRazorService : IServicesConfigurator
+  {
+    public void Configure(IServiceCollection serviceCollection)
+    {
+      serviceCollection.AddSingleton<IRazorService, Sitecore.Support.XA.Foundation.SitecoreExtensions.Services.RazorService>();
+    }
+  }
+}
